502f6a2e969c3a0dc5826e2db023380c1a7d467c
“milindkusahu” <milindsahu2000@gmail.com>
2024-06-08 14:44:17 +0530

    updated

--- session-1/index.js
+++ session-1/index.js
@@ -1,7 +1,24 @@
 const http = require("http")
 
-const server = http.createServer(() => {
-    console.log("request received")
+const server = http.createServer((request, response) => {
+
+    const serverInfo = {
+        serverName: "Crio Server",
+        version: "1.0.0",
+        currentDate: new Date().toDateString(),
+        currentTime: new Date().toTimeString(),
+    };
+
+    if (request.url === "/status") {
+        response.writeHead(200, {"Content-Type": "application/json"});
+        response.write(JSON.stringify(serverInfo))
+        response.end();
+
+    } else {
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(`<h1>Hello from Server</h1>`);
+        response.end();
+    }
 });
 
 server.listen(8082, () => {
