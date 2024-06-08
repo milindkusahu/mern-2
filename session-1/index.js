ee784da2679bbbf0f6291dbf52060a5963577374
“milindkusahu” <milindsahu2000@gmail.com>
2024-06-08 15:18:43 +0530

    update

--- session-1/index.js
+++ session-1/index.js
@@ -1,28 +1,41 @@
-const http = require("http")
+const http = require("http");
+const currenciesData = require("./currencies.json");
+const PORT = 8082;
+
+// const server = http.createServer((request, response) => {
+
+//     const serverInfo = {
+//         serverName: "Crio Server",
+//         version: "1.0.0",
+//         currentDate: new Date().toDateString(),
+//         currentTime: new Date().toTimeString(),
+//     };
+
+//     if (request.url === "/status") {
+//         response.writeHead(200, {"Content-Type": "application/json"});
+//         // response.write(JSON.stringify(serverInfo))
+//         // response.end();
+//         // or
+//         response.end(JSON.stringify(serverInfo))
+
+//     } else {
+//         response.writeHead(200, {"Content-Type": "text/html"});
+//         response.write(`<h1>Hello from Server</h1>`);
+//         response.end();
+//     }
+// });
 
 const server = http.createServer((request, response) => {
 
-    const serverInfo = {
-        serverName: "Crio Server",
-        version: "1.0.0",
-        currentDate: new Date().toDateString(),
-        currentTime: new Date().toTimeString(),
-    };
-
-    if (request.url === "/status") {
-        response.writeHead(200, {"Content-Type": "application/json"});
-        // response.write(JSON.stringify(serverInfo))
-        // response.end();
-        // or
-        response.end(JSON.stringify(serverInfo))
-
-    } else {
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write(`<h1>Hello from Server</h1>`);
-        response.end();
+    switch (request.url) {
+        case "/": response.writeHead(200, {"Content-Type": "text/html"}).end(`<h1>Hello from Server</h1>`);
+        break;
+        case "/currencies": response.writeHead(200, {"Content-Type": "application/json"}).end(JSON.stringify(currenciesData.data));
+        break;
+        default: response.writeHead(404, { "Content-Type": "application/json" }).end(JSON.stringify({message: "Not found" }));
     }
 });
 
-server.listen(8082, () => {
+server.listen(PORT, () => {
     console.log("Server is listening now...")
 })
