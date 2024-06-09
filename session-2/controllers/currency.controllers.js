97685d40bbec0fe62939a14002d023c6a149f74d
“milindkusahu” <milindsahu2000@gmail.com>
2024-06-09 18:04:18 +0530

    updated session-2

--- session-2/controllers/currency.controllers.js
+++ session-2/controllers/currency.controllers.js
@@ -1,25 +1,42 @@
-const currenciesData = require("./currencies.json");
+const axios = require("axios");
+// const currenciesData = require("./currencies.json");
 
-const getCurrencies = (req, res) => {
+const getCurrencies = async (req, res) => {
   const { min_value } = req.query;
-  if (min_value)
-    return res.send(
-      currenciesData.data.filter(({ min_size }) => min_size === min_value)
+  try {
+    const currenciesData = await axios.get(
+      "https://api.coinbase.com/v2/currencies"
     );
-  res.send(currenciesData.data);
+    if (min_value)
+      return res.send(
+        currenciesData.data.data.filter(
+          ({ min_size }) => min_size === min_value
+        )
+      );
+    res.send(currenciesData.data.data);
+  } catch (error) {
+    res.status(505).send({ message: "Problems in fetching data, retry!" });
+  }
 };
 
-const getCurrencyBySymbol = (req, res) => {
+const getCurrencyBySymbol = async (req, res) => {
   // console.log(req.params.symbol);
   // console.log(req.query);
   const { symbol } = req.params;
-  const reqCurrency = currenciesData.data.find(
-    ({ id }) => id.toLowerCase() === symbol.toLowerCase()
-  );
+  try {
+    const currenciesData = await axios.get(
+      "https://api.coinbase.com/v2/currencies"
+    );
+    const reqCurrency = currenciesData.data.data.find(
+      ({ id }) => id.toLowerCase() === symbol.toLowerCase()
+    );
 
-  if (reqCurrency === undefined)
-    return res.status(404).send({ message: "Invalid Currency Symbol" });
-  res.send(reqCurrency);
+    if (reqCurrency === undefined)
+      return res.status(404).send({ message: "Invalid Currency Symbol" });
+    res.send(reqCurrency);
+  } catch (error) {
+    res.status(505).send({ message: "Problems in fetching data, retry!" });
+  }
 };
 
 module.exports = { getCurrencies, getCurrencyBySymbol };
